f015fa98c4931a8cab67421f9eef43f49c245acb
theinspireindiaofficial-hue <theinspireindiaofficial@gmail.com>
2026-06-15 14:09:13 +0530

    Update BusinessInsights.tsx

--- src/pages/BusinessInsights.tsx
+++ src/pages/BusinessInsights.tsx
@@ -1,6 +1,6 @@
 import Layout from "@/components/Layout";
 import { motion } from "framer-motion";
-import { businessInsights } from "../data/businessinsights";
+import { businessinsights } from "../data/businessinsights";
 
 const BusinessInsights = () => {
   return (
